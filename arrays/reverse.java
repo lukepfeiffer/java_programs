f692d9686c0882e11a15201c9cba630612ea9578
Hashrocket Workstation <dev@hashrocket.com>
2016-12-08 19:07:19 -0500

    Inital commit with reverse method

---
+++ arrays/reverse.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.io.*;
+class ReverseArray{
+  public static void main(String args[]){
+    System.out.println("Input your number and press enter: ");
+    Scanner input = new Scanner(System.in);
+    int length = 4;
+    int values[] = new int[length];
+
+    // populate variable
+    for(int i = 0; i < length; ++i) { values[i] = input.nextInt(); }
+    for(int i = length - 1; i >= 0; --i) { System.out.print(values[i] + " "); }
+  }
+}
