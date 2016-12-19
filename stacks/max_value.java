a61fdbd78b635881f65166731ef8cfd5149c1181
Hashrocket Workstation <dev@hashrocket.com>
2016-12-19 17:56:55 -0500

    Organize programs

---
+++ stacks/max_value.java
@@ -0,0 +1,9 @@
+import java.util.*;
+import java.io.*;
+
+class MaxValue{
+  public static void main(String arg[]){
+    System.out.println("Enter some numbers");
+    Stack myStack = new Stack();
+  }
+}
