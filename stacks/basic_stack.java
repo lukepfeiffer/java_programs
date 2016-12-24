066c2d51392fcb955a68ea8cc14f579d542dfa56
Hashrocket Workstation <dev@hashrocket.com>
2016-12-23 21:45:54 -0500

    Basic stack implementation

---
+++ stacks/basic_stack.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.io.*;
+
+class BasicStack {
+  public static void main(String args[]){
+    Stack<Integer> myStack = new Stack<Integer>();
+    myStack.push(6);
+    System.out.print(myStack);
+  }
+}
--- stacks/max_value.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-import java.io.*;
-
-class MaxValue{
-  public static void main(String arg[]){
-    System.out.println("Enter some numbers");
-    Stack myStack = new Stack();
-  }
-}
