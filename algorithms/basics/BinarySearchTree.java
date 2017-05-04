201b3db897755b4b59ccc1ffc7b33127000a0fdb
Luke Pfeiffer <lukerpfeiffer@gmail.com>
2017-05-04 15:57:15 -0400

    Update

---
+++ algorithms/basics/BinarySearchTree.java
@@ -0,0 +1,32 @@
+import java.util.*;
+import java.io.*;
+public class BinarySearchTree{
+  public static void main(String args[]){
+
+    boolean flag = true;
+    int randomInt = (int) (Math.random() * 300 + 1);
+    int [] array = new int[300];
+    int max = 300;
+    int min = 0;
+    int numGuesses = 0;
+
+    for(int i = 0; i < array.length; ++i){
+      array[i] = i + 1;
+    }
+
+    while(flag){
+      int compGuess = (max + min) / 2;
+      ++numGuesses;
+      if( compGuess < randomInt){
+        min = compGuess + 1;
+      } else if( compGuess > randomInt){
+        max = compGuess - 1;
+      } else if(compGuess == randomInt){
+        flag = false;
+      }
+    }
+
+    System.out.println("The random number was: " + randomInt);
+    System.out.println("The number of guesses was: " + numGuesses);
+  }
+}
--- cop3502/lecture_demos/sandbox.java
+++ cop3502/lecture_demos/sandbox.java
@@ -3,19 +3,27 @@ import java.io.*;
 
 class SandBox{
   public static void main(String[] args){
-                String returnString = "";
-		String meridian = "";
-		if (22.0987 >= 12){
-			meridian = "PM";
-		} else {
-			meridian = "AM";
-		}
-		double newTime = ((int)(22.0987 * 100)/100.0);
-		String stringTime = Double.toString(newTime);
-		String[] split = stringTime.split("\\.");
-		String hour = split[0];
-		String minutes = split[1];
-		returnString = hour + ":" + minutes + " " + meridian;
-		System.out.println(returnString);
+    Dog dog = new Dog();
+    Boxer boxer = new Boxer();
+    dog = (Dog)((Animal)boxer);
+    System.out.println(dog instanceof Boxer);
+  }
+}
+
+class Animal {
+  void eat(){
+    System.out.println("Animal is eating");
+  }
+}
+
+class Dog extends Animal{
+  void eat(){
+    System.out.println("Dog is eating");
+  }
+}
+
+class Boxer extends Dog{
+  void eat(){
+    System.out.println("Boxer is eating");
   }
 }
